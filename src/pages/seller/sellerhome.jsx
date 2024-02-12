d8237590c70eb392e2f71a496c2cf3ac5ffea6dd
Barath <barathkumarj121@gmail.com>
2024-02-12 12:13:01 +0530

    sellerpage backend updated

--- src/pages/seller/sellerhome.jsx
+++ src/pages/seller/sellerhome.jsx
@@ -1,3 +1,6 @@
+import { list } from 'postcss';
+import React, { useState, useEffect } from 'react';
+
 function sellerhome() {
 
     // Sellers page 
@@ -8,11 +11,89 @@ function sellerhome() {
     // c) join community or allot a community 
     // d) profile , sale , finance report 
     
+// const apiurl="https://example-to9v.onrender.com/add/10";
+
+const [sellers, setSellers] = useState([]);
+const [number,setnumber]=useState(0);
+// useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(apiurl);
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         const json = await response.json();
+//         console.log(response);
+//         setSellers(json);
+//       } catch (error) {
+//         console.error('There was an error!', error);
+//       } finally {
+//         console.log('finally');
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+const scheduleapi="https://serious.onrender.com/schedule/";
+const [schedule, setSchedule] = useState([]);
+
+  function addnumber(number){
+    const apiurl="https://example-to9v.onrender.com/add/"+number;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiurl);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const json = await response.json();
+        
+        setSellers(json);
+        console.log(sellers);
+      } catch (error) {
+        console.error('There was an error!', error);
+      } finally {
+        console.log('finally');
+      }
+    };
+    fetchData();
+    }
+    function scheduledetails(){
+      
+      const fetchData = async () => {
+        try {
+          const response = await fetch(scheduleapi);
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const json = await response.json();
+          console.log(response);
+          setSchedule(json);
+        } catch (error) {
+          console.error('There was an error!', error);
+        } finally {
+          console.log('finally');
+        }
+      };
+      fetchData(),[];
+      }
+
+
+
+    
+
 
     return (
         <div>
             <h1>Seller Home</h1>
+            
+            <input  onChange={(e) => setnumber(e.target.value)} type='number' placeholder='Enter the number' />
+            <button onClick={() => addnumber(number)} className=" border-2 ">Add number</button>
+            <pre>{sellers.result}</pre>
+            <button onClick={() => scheduledetails()} className=" border-2 ">Get Schedule</button>
+            <pre>{schedule.schedule}</pre>
+
         </div>
+
     )
 }
 
